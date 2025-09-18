efad3d660d6ca6871832e7b5cacb914c3265ea05
mahauni <lucasraoniha@gmail.com>
2025-09-18 18:38:38 -0300

    Changing font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,12 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.font = wezterm.font_with_fallback({
+	"FiraMono Nerd Font",
+	"JetBrains Mono",
+})
+config.font_size = 13
+
 config.enable_tab_bar = false
 config.window_background_opacity = 0.13
 config.window_background_image = "/home/mahauni/.config/wezterm/assets/frieren.jpeg"

b5f7bb1b937bf9ad884b8721a39a9509fb474297
mahauni <lucasraoniha@gmail.com>
2025-09-17 21:51:54 -0300

    Fixing some wezterm styling

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,12 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.enable_tab_bar = false
-config.window_background_opacity = 0.15
+config.window_background_opacity = 0.13
 config.window_background_image = "/home/mahauni/.config/wezterm/assets/frieren.jpeg"
+
+-- maybe latter change the red of the Tokyo Night to the Gruber
+config.color_scheme = "Tokyo Night"
+-- config.color_scheme = "Gruber (base16)"
+-- config.color_scheme = "Catppuccin Macchiato"
 
 return config

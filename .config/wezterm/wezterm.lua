e4bd1645b5ac78098a56aadb78b59b0a463eed7e
mahauni <lucasraoniha@gmail.com>
2025-11-18 20:29:03 -0300

    Fixing wezterm to work with windows and adjusting p10k

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,17 +1,36 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+-- Fonts
 config.font = wezterm.font_with_fallback({
+	-- "MesloLGS NF",
 	"FiraMono Nerd Font",
 	"JetBrains Mono",
 })
 config.font_size = 13
 
-config.enable_tab_bar = false
-config.window_background_opacity = 0.13
-config.window_background_image = "/home/mahauni/.config/wezterm/assets/frieren.jpeg"
+-- Windows
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	Default_prog = { "powershell.exe" }
+	config.window_background_image = weztterm.home_dir .. "/wezterm/assets/frieren.jpeg"
+	config.window_background_hsb = {
+		hue = 1.0,
+		saturation = 1.0,
+		brightness = 0.05,
+	}
+end
 
--- maybe latter change the red of the Tokyo Night to the Gruber
+-- Linux
+if wezterm.target_triple == "x86_64-unknown-linux-gnu" then
+	config.window_background_opacity = 0.13
+	config.window_background_image = wezterm.home_dir .. "/.config/wezterm/assets/frieren.jpeg"
+end
+
+-- Configs
+config.enable_tab_bar = false
+config.default_prog = Default_prog
+
+-- Colorscheme
 config.color_scheme = "Tokyo Night"
 -- config.color_scheme = "Gruber (base16)"
 -- config.color_scheme = "Catppuccin Macchiato"
